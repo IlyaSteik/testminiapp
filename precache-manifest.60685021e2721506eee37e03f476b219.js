bf2ccf36d409a547766a5bf042cf995c43372773
Admin <ilya.steik4@gmail.com>
2020-04-29 13:30:09 +0300

    Updates

---
+++ precache-manifest.60685021e2721506eee37e03f476b219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adda676770549583b005ac04e5db203",
+    "url": "/testminiapp/index.html"
+  },
+  {
+    "revision": "7ca65a747d4d13785537",
+    "url": "/testminiapp/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "0a91d11e7371eb12cea8",
+    "url": "/testminiapp/static/css/main.82128725.chunk.css"
+  },
+  {
+    "revision": "7ca65a747d4d13785537",
+    "url": "/testminiapp/static/js/2.d1e33334.chunk.js"
+  },
+  {
+    "revision": "0a91d11e7371eb12cea8",
+    "url": "/testminiapp/static/js/main.3cece0b8.chunk.js"
+  },
+  {
+    "revision": "b497ed493a9bbe83b9d5",
+    "url": "/testminiapp/static/js/runtime-main.a6f6e525.js"
+  }
+]);
